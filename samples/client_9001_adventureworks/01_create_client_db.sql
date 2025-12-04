79377a09596256b8c7484199df1d2176d917c1da
Jorge Prax <admin@sintelo.com>
2025-12-04 12:16:56 -0600

    Update 01_create_client_db.sql

--- samples/client_9001_adventureworks/01_create_client_db.sql
+++ samples/client_9001_adventureworks/01_create_client_db.sql
@@ -1,1 +1,16 @@
+/*
+------------------------------------------------------------
+Client 9001 – AdventureWorks
+Database Provisioning Script
+------------------------------------------------------------
+*/
 
+CREATE DATABASE CLIENT_9001_ADVENTUREWORKS;
+GO
+
+USE CLIENT_9001_ADVENTUREWORKS;
+GO
+
+CREATE SCHEMA raw;
+CREATE SCHEMA sintelo_T1;
+CREATE SCHEMA sintelo_T2;
